86b1d7e6026fddd98246e5679073f18608253183
mepux <patrick.meier@gmx.net>
2023-06-20 17:19:38 +0200

    Sort order issue

--- src/test/java/com/github/toolarium/system/command/process/util/ScriptUtilTest.java
+++ src/test/java/com/github/toolarium/system/command/process/util/ScriptUtilTest.java
@@ -114,7 +114,9 @@ public class ScriptUtilTest {
 
         list = ScriptUtil.getInstance().selectInvalidProcessDirectories(pidFileFolder, 100, 500);
         Collections.sort(list);
-        assertEquals(Arrays.asList(pidFile1.getParent(), pidFile2.getParent(), pidFile4.getParent()), list);
+        List<Path> referenceList = Arrays.asList(pidFile1.getParent(), pidFile2.getParent(), pidFile4.getParent());
+        Collections.sort(referenceList);
+        assertEquals(referenceList, list);
     }
 }
 
